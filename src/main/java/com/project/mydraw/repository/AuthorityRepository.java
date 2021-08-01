054edd919b51c758df23049f70a8d41b18dced65
Aespartal <alejandro.espartal@nunsys.com>
2021-08-01 21:13:49 +0200

    Initial version of mydraw generated by JHipster-6.10.5

---
+++ src/main/java/com/project/mydraw/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.project.mydraw.repository;
+
+import com.project.mydraw.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
---
+++ src/main/java/com/project/mydraw/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.project.mydraw.service.dto;
